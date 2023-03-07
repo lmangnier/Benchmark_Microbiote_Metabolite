f7b2f1fe9f8d1afeb4976ba0ad51f750c3b39eea
lmangnier <loic.mangnier@crchudequebec.ulaval.ca>
2023-03-07 13:19:39 -0500

    Add performance metrics for correlation/regression methods

--- code/correlation_regression_scenarios_25_25.R
+++ code/correlation_regression_scenarios_25_25.R
@@ -1,90 +1,16 @@
 #Code to assess Correlation and Regression-based approaches for scenarios 25-25 
 
+load("util_functions.R")
+
 #Correlation approaches:
 #Spearman correlation
+list.scenarios.dense.Microbiotes = readRDS("../data_scenarios_denses_fixed_Microbiotes.RDS")
+list.scenarios.dense.Metabolites = readRDS("../data_scenarios_denses_fixed_Metabolites.RDS")
+list.scenarios.dense.index = readRDS("../data_scenarios_denses_fixed_index.RDS")
 
-list.scenarios.dense.Microbiotes = readRDS("C:\\Users\\loicm\\Desktop\\Projets\\Mimint\\data\\data_scenarios_denses_fixed_Microbiotes.RDS")
-list.scenarios.dense.Metabolites = readRDS("C:\\Users\\loicm\\Desktop\\Projets\\Mimint\\data\\data_scenarios_denses_fixed_Metabolites.RDS")
-list.scenarios.dense.index = readRDS("C:\\Users\\loicm\\Desktop\\Projets\\Mimint\\data\\data_scenarios_denses_fixed_index.RDS")
-
-list.scenarios.dense.random.Microbiotes = readRDS("C:\\Users\\loicm\\Desktop\\Projets\\Mimint\\data\\data_scenarios_denses_random_Microbiotes.RDS")
-list.scenarios.dense.random.Metabolites = readRDS("C:\\Users\\loicm\\Desktop\\Projets\\Mimint\\data\\data_scenarios_denses_random_Metabolites.RDS")
-list.scenarios.dense.random.index = readRDS("C:\\Users\\loicm\\Desktop\\Projets\\Mimint\\data\\data_scenarios_denses_random_index.RDS")
-
-compute.pairwise.spearman = function(X,Y){
-  
-  ncol.X = ncol(X)
-  ncol.Y = ncol(Y)
-  
-  lapply(seq_len(ncol.X), function(x){
-    lapply(seq_len(ncol.Y), function(y){
-      cor.test(X[,x], Y[,y], method="spearman")
-    })
-  })
-  
-}
-
-#Assess global association by microbiote, while adjusting for multiplicity (ACAT)
-ACAT.by.element = function(cor.test.by.element, X, Y){
-  
-  ncol.X = ncol(X)
-  ncol.Y = ncol(Y)
-    
-  sapply(seq_len(ncol.X), function(x){
-    ACAT::ACAT(sapply(seq_len(ncol.Y), function(y){
-      cor.test.by.element[[x]][[y]]$p.value
-    }))
-  })
-}
-
-#Compute Jaccard index
-compute.jaccard = function(p.values, index.true.associated, threshold.signi=0.05){
-  
-  index.signi.p.values = which(p.values<=threshold.signi)
-  
-  sum(index.signi.p.values%in%index.true.associated)/
-    (length(index.signi.p.values)+length(index.true.associated)-sum(index.signi.p.values%in%index.true.associated))
-}
-
-find.true.associations = function(cor.test.by.element,threshold.signi=0.05, index.X, index.Y){
-  titi = lapply(cor.test.by.element, function(x) {
-    which(sapply(x, function(y){
-      y$p.value<=threshold.signi
-    }))
-  })
-  
-  pairs = list()
-  
-  for(i in 1:length(index.X)){
-    a = c(index.X[i],
-          index.Y[i])
-    pairs[[i]] = a
-  }
-  
-  index.non.null = which(sapply(titi, function(x) length(x))>0)
-  
-  l=c()
-  
-  for(i in index.non.null){
-    
-    u = c()
-    for(j in titi[[i]]){
-      c = c(i,j)
-      n = c()
-      for(p in 1:length(index.X)){
-        n = c(n,all(c  == pairs[[p]]))
-      }
-      
-      u = c(u,sum(n))
-      
-    }
-    
-    l = c(l,sum(n))
-  }
-  
-  sum(l)/(length(index.X)+length(l)-sum(l))
-}
-
+list.scenarios.dense.random.Microbiotes = readRDS("../data_scenarios_denses_random_Microbiotes.RDS")
+list.scenarios.dense.random.Metabolites = readRDS("../data_scenarios_denses_random_Metabolites.RDS")
+list.scenarios.dense.random.index = readRDS("../data_scenarios_denses_random_index.RDS")
 
 #For a same number of associated elements (3 microbiotes associated with 3 metabolites)
 #Beta=-0.5
@@ -135,6 +61,9 @@ boxplot(jaccard.Scenario3_beta_neg0.5,jaccard.Scenario3_beta_neg0.25,jaccard.Sce
         names=c("Neg 0.5", "Neg 0.25", "0.25", "0.5", "Mixed"), horizontal=F)
 
 
+#Confusion Matrix:
+
+
 #We can do the same now comparing different number of associated elements for the same level of association
 #Let's take our mixed scenario as main results
 spearman.cor.Scenario_1_beta_mixed = lapply(1:1000, function(rep) {
@@ -155,6 +84,65 @@ jaccard.Scenario10_beta_mixed = sapply(1:1000, function(rep) {
 
 boxplot(jaccard.Scenario1_beta_mixed,jaccard.Scenario3_beta_mixed,jaccard.Scenario10_beta_mixed, names=c(1,3,10))
 
+
+
+#Confusion Matrix, Sensitivity, Specificity, F1-Score
+pvalues.ACAT.Scenario10_beta_mixed = lapply(1:1000, function(rep){
+  ACAT.by.element(spearman.cor.Scenario_10_beta_mixed[[rep]], list.scenarios.dense.random.Microbiotes$Scenario_10_beta_random[[rep]],list.scenarios.dense.random.Metabolites$Scenario_10_beta_random[[rep]])
+})
+
+pvalues.ACAT.Scenario3_beta_mixed = lapply(1:1000, function(rep){
+  ACAT.by.element(spearman.cor.Scenario_3_beta_mixed[[rep]], list.scenarios.dense.random.Microbiotes$Scenario_3_beta_random[[rep]],list.scenarios.dense.random.Metabolites$Scenario_3_beta_random[[rep]])
+})
+
+pvalues.ACAT.Scenario1_beta_mixed = lapply(1:1000, function(rep){
+  ACAT.by.element(spearman.cor.Scenario_1_beta_mixed[[rep]], list.scenarios.dense.random.Microbiotes$Scenario_1_beta_random[[rep]],list.scenarios.dense.random.Metabolites$Scenario_1_beta_random[[rep]])
+})
+
+
+boxplot(sapply(1:1000, function(rep) f1.score(confusion.matrix(pvalues.ACAT.Scenario1_beta_mixed[[rep]], list.scenarios.dense.random.index$Scenario_1_beta_random[[rep]]$Microbiotes, c(1:25)[-list.scenarios.dense.random.index$Scenario_1_beta_random[[rep]]$Microbiotes], 0.002))),
+        sapply(1:1000, function(rep) f1.score(confusion.matrix(pvalues.ACAT.Scenario3_beta_mixed[[rep]], list.scenarios.dense.random.index$Scenario_3_beta_random[[rep]]$Microbiotes, c(1:25)[-list.scenarios.dense.random.index$Scenario_3_beta_random[[rep]]$Microbiotes], 0.002))),
+        sapply(1:1000, function(rep) f1.score(confusion.matrix(pvalues.ACAT.Scenario10_beta_mixed[[rep]], list.scenarios.dense.random.index$Scenario_10_beta_random[[rep]]$Microbiotes, c(1:25)[-list.scenarios.dense.random.index$Scenario_10_beta_random[[rep]]$Microbiotes], 0.002))))
+
+#If we are interested in constructing ROC curve.
+build.ROC(pvalues.ACAT.Scenario10_beta_mixed[[577]], list.scenarios.dense.random.index$Scenario_10_beta_random[[577]]$Microbiotes, c(1:25)[-list.scenarios.dense.random.index$Scenario_10_beta_random[[577]]$Microbiotes])
+
+#Compute AUC
+auc.Scenario10_beta_mixed = sapply(1:1000, function(x) compute.auc(pvalues.ACAT.Scenario10_beta_mixed[[x]],list.scenarios.dense.random.index$Scenario_10_beta_random[[x]]$Microbiotes))
+auc.Scenario3_beta_mixed = sapply(1:1000, function(x) compute.auc(pvalues.ACAT.Scenario3_beta_mixed[[x]],list.scenarios.dense.random.index$Scenario_3_beta_random[[x]]$Microbiotes))
+auc.Scenario1_beta_mixed = sapply(1:1000, function(x) compute.auc(pvalues.ACAT.Scenario1_beta_mixed[[x]],list.scenarios.dense.random.index$Scenario_1_beta_random[[x]]$Microbiotes))
+
+
+boxplot(auc.Scenario1_beta_mixed,auc.Scenario3_beta_mixed,auc.Scenario10_beta_mixed)
+
+#Comparisons with other association levels:
+pvalues.ACAT.Scenario3_beta_neg0.5 = lapply(1:1000, function(rep){
+  ACAT.by.element(spearman.cor.Scenario_3_beta_neg0.5[[rep]], list.scenarios.dense.Microbiotes$`Scenario_3_beta_-0.5`[[rep]],list.scenarios.dense.Metabolites$`Scenario_3_beta_-0.5`[[rep]])
+})
+
+pvalues.ACAT.Scenario3_beta_neg0.25 = lapply(1:1000, function(rep){
+  ACAT.by.element(spearman.cor.Scenario_3_beta_neg0.25[[rep]], list.scenarios.dense.Microbiotes$`Scenario_3_beta_-0.25`[[rep]],list.scenarios.dense.Metabolites$`Scenario_3_beta_-0.25`[[rep]])
+})
+
+pvalues.ACAT.Scenario3_beta_0.25 = lapply(1:1000, function(rep){
+  ACAT.by.element(spearman.cor.Scenario_3_beta_0.25[[rep]], list.scenarios.dense.Microbiotes$Scenario_3_beta_0.25[[rep]],list.scenarios.dense.Metabolites$Scenario_3_beta_0.25[[rep]])
+})
+
+pvalues.ACAT.Scenario3_beta_0.5 = lapply(1:1000, function(rep){
+  ACAT.by.element(spearman.cor.Scenario_3_beta_0.5[[rep]], list.scenarios.dense.Microbiotes$Scenario_3_beta_0.5[[rep]],list.scenarios.dense.Metabolites$Scenario_3_beta_0.5[[rep]])
+})
+
+auc.Scenario3_beta_neg0.5 = sapply(1:1000, function(x) compute.auc(pvalues.ACAT.Scenario3_beta_neg0.5[[x]],list.scenarios.dense.index$`Scenario_3_beta_-0.5`[[x]]$Microbiotes))
+auc.Scenario3_beta_neg0.25 = sapply(1:1000, function(x) compute.auc(pvalues.ACAT.Scenario3_beta_neg0.25[[x]],list.scenarios.dense.index$`Scenario_3_beta_-0.25`[[x]]$Microbiotes))
+auc.Scenario3_beta_0.25 = sapply(1:1000, function(x) compute.auc(pvalues.ACAT.Scenario3_beta_0.25[[x]],list.scenarios.dense.index$Scenario_3_beta_0.25[[x]]$Microbiotes))
+auc.Scenario3_beta_0.5 = sapply(1:1000, function(x) compute.auc(pvalues.ACAT.Scenario3_beta_0.5[[x]],list.scenarios.dense.index$Scenario_3_beta_0.5[[x]]$Microbiotes))
+
+boxplot(auc.Scenario3_beta_neg0.5,
+        auc.Scenario3_beta_neg0.25,
+        auc.Scenario3_beta_0.25,
+        auc.Scenario3_beta_0.5,
+        auc.Scenario3_beta_mixed)
+
 jaccard.associations.Scenario1_beta_mixed = sapply(1:1000, function(rep) find.true.associations(spearman.cor.Scenario_1_beta_mixed[[rep]], 0.05/25, list.scenarios.dense.random.index$Scenario_1_beta_random[[rep]]$Microbiotes,list.scenarios.dense.random.index$Scenario_1_beta_random[[rep]]$Metabolites))
 jaccard.associations.Scenario3_beta_mixed = sapply(1:1000, function(rep) find.true.associations(spearman.cor.Scenario_3_beta_mixed[[rep]], 0.05/25, list.scenarios.dense.random.index$Scenario_3_beta_random[[rep]]$Microbiotes,list.scenarios.dense.random.index$Scenario_3_beta_random[[rep]]$Metabolites))
 jaccard.associations.Scenario10_beta_mixed = sapply(1:1000, function(rep) find.true.associations(spearman.cor.Scenario_10_beta_mixed[[rep]], 0.05/25, list.scenarios.dense.random.index$Scenario_10_beta_random[[rep]]$Microbiotes,list.scenarios.dense.random.index$Scenario_10_beta_random[[rep]]$Metabolites))
@@ -172,9 +160,15 @@ boxplot(jaccard.associations.Scenario3_beta_neg0.5,jaccard.associations.Scenario
 summary(jaccard.associations.Scenario3_beta_0.25)
 
 #Regression approaches:
+#Here we use ZINB approach:
 library(pscl)
 
-l.r = list()
+
+zinb.Scenario3_beta_neg0.5 = list()
+zinb.Scenario3_beta_neg0.25 = list()
+zinb.Scenario3_beta_0.25 = list()
+zinb.Scenario3_beta_0.5 = list()
+zinb.Scenario3_beta_mixed = list()
 
 for(r in 1:100){
   print(r)
---
+++ code/util_functions.R
@@ -0,0 +1,122 @@
+#Util functions
+
+compute.pairwise.spearman = function(X,Y){
+  
+  ncol.X = ncol(X)
+  ncol.Y = ncol(Y)
+  
+  lapply(seq_len(ncol.X), function(x){
+    lapply(seq_len(ncol.Y), function(y){
+      cor.test(X[,x], Y[,y], method="spearman")
+    })
+  })
+  
+}
+
+#Assess global association by microbiote, while adjusting for multiplicity (ACAT)
+ACAT.by.element = function(cor.test.by.element, X, Y){
+  
+  ncol.X = ncol(X)
+  ncol.Y = ncol(Y)
+  
+  sapply(seq_len(ncol.X), function(x){
+    ACAT::ACAT(sapply(seq_len(ncol.Y), function(y){
+      cor.test.by.element[[x]][[y]]$p.value
+    }))
+  })
+}
+
+#Compute Jaccard index
+compute.jaccard = function(p.values, index.true.associated, threshold.signi=0.05){
+  
+  index.signi.p.values = which(p.values<=threshold.signi)
+  
+  sum(index.signi.p.values%in%index.true.associated)/
+    (length(index.signi.p.values)+length(index.true.associated)-sum(index.signi.p.values%in%index.true.associated))
+}
+
+find.true.associations = function(cor.test.by.element,threshold.signi=0.05, index.X, index.Y){
+  titi = lapply(cor.test.by.element, function(x) {
+    which(sapply(x, function(y){
+      y$p.value<=threshold.signi
+    }))
+  })
+  
+  pairs = list()
+  
+  for(i in 1:length(index.X)){
+    a = c(index.X[i],
+          index.Y[i])
+    pairs[[i]] = a
+  }
+  
+  index.non.null = which(sapply(titi, function(x) length(x))>0)
+  
+  l=c()
+  
+  for(i in index.non.null){
+    
+    u = c()
+    for(j in titi[[i]]){
+      c = c(i,j)
+      n = c()
+      for(p in 1:length(index.X)){
+        n = c(n,all(c  == pairs[[p]]))
+      }
+      
+      u = c(u,sum(n))
+      
+    }
+    
+    l = c(l,sum(n))
+  }
+  
+  sum(l)/(length(index.X)+length(l)-sum(l))
+}
+
+confusion.matrix = function(pvalues, true.associated, true.non.associated ,signi.threshold=0.05){
+  
+  significant.associations = which(pvalues<=signi.threshold)
+  non.significant.associations = which(pvalues>signi.threshold)
+  
+  TP = sum(significant.associations%in%true.associated)
+  TN = sum(non.significant.associations%in%true.non.associated)
+  FP = sum(significant.associations%in%true.non.associated)
+  FN = sum(non.significant.associations%in%true.associated)
+  
+  return(matrix(c(TP, FN,FP, TN), nrow=2, ncol=2, dimnames = list(c("Pred_Pos","Pred_Neg"), c("True_Pos","True_Neg"))))
+}
+
+sensivity.specificity.from.confusion.matrix = function(confusion.matrix){
+  
+  sensitivity = confusion.matrix[1,1]/(confusion.matrix[1,1]+confusion.matrix[2,1])
+  specificity = confusion.matrix[2,2]/(confusion.matrix[2,2]+confusion.matrix[1,2])
+  
+  return(c(sensitivity,specificity))
+}
+       
+f1.score = function(confusion.matrix){
+  m = sensivity.specificity.from.confusion.matrix(confusion.matrix)
+  
+  return(2/((1/m[1])+(1/m[2])))
+}       
+
+build.ROC = function(pvalues, true.associated, true.non.associated, signi.levels = seq(0,1, 0.0001)){
+  
+  confusion.matrix.for.all.signi = lapply(signi.levels, function(rho) confusion.matrix(pvalues,true.associated,true.non.associated,rho))
+  
+  specificity.sensitivity.for.all.confusion.matrix = sapply(confusion.matrix.for.all.signi, function(x){
+    sensivity.specificity.from.confusion.matrix(x)
+  })
+  
+  plot(1-specificity.sensitivity.for.all.confusion.matrix[2,],specificity.sensitivity.for.all.confusion.matrix[1,], xlab="1-Specificity", ylab="Sensitivity")
+  lines(1-specificity.sensitivity.for.all.confusion.matrix[2,],specificity.sensitivity.for.all.confusion.matrix[1,], lwd=2)
+  abline(0,1, lty=2, lwd=2)
+}
+
+compute.auc = function(pvalues,true.associated){
+  ert = 1:length(pvalues)
+  
+  ert = as.numeric(ert%in%true.associated)
+  auc(roc(ert,pvalues))
+}
